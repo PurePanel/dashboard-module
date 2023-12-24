3a9e170417a30eebe9de5195986371d12b493b18
Fatih Alp <fatihalp89@gmail.com>
2023-12-24 21:04:33 +0300

    Version update

--- src/Widget/Extension/WidgetExtension.php
+++ src/Widget/Extension/WidgetExtension.php
@@ -48,7 +48,7 @@ class WidgetExtension extends Extension implements WidgetExtensionInterface
         $this->load($widget);
         $this->content($widget);
 
-        return $this->dispatch(new GetOutput($widget));
+        return dispatch_sync(new GetOutput($widget));
     }
 
     /**
@@ -68,7 +68,7 @@ class WidgetExtension extends Extension implements WidgetExtensionInterface
      */
     protected function content(WidgetInterface $widget)
     {
-        $this->dispatch(new SetContent($widget));
+        dispatch_sync(new SetContent($widget));
     }
 
     /**
